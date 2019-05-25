a92f36705fa1dfa6f07564bbf399ae58b3a11770
Kieran Weaver <drawbook.productions@gmail.com>
2019-05-25 10:10:23 -0700

    added DDS support, enabled blending

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ CXX=g++
 #CXXFLAGS=-DUSE_ZLIB -Og -g -fsanitize=address,undefined -march=native -std=c++11
 CXXFLAGS=-DUSE_ZLIB -O2 -Wall -Wextra -pedantic -march=native -std=c++11
 OBJS=main.o gl31.o GL/Helpers.o GL/Shader.o GL/Camera.o
-LDFLAGS=-lGL -lglfw -ldl
+LDFLAGS=-lGL -lglfw -ldl -lz
 all: $(OBJS)
 	$(CXX) $(CXXFLAGS) $(OBJS) -o Platformer $(LDFLAGS)
 clean:
--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 #include "gl31.h"
 #include <GLFW/glfw3.h>
 #include <iostream>
-#define STB_IMAGE_IMPLEMENTATION
-#include "stb/stb_image.h"
+#include <zlib.h>
+#include <cstring>
 #include "GL/Shader.hpp"
 #include "GL/Camera.hpp"
+struct PODvec2{
+	float x;
+	float y;
+};
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods){
 	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS){
 		glfwSetWindowShouldClose(window,GLFW_TRUE);
@@ -22,6 +26,62 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 		}
 	}
 }
+int loadDDSgz(const std::string& path,GLuint& m_texture){
+	glBindTexture(GL_TEXTURE_2D,m_texture);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	gzFile gzhandle = gzopen(path.c_str(),"rb");
+	char * header = new char[129];
+	gzread(gzhandle, header, 128);
+	header[128] = '\0';
+	if (strncmp(header,"DDS ",4)!=0){
+		delete[] header;
+		gzclose(gzhandle);
+		return -1;
+	}else{
+		uint32_t height = *reinterpret_cast<uint32_t*>(header+12);
+		uint32_t width = *reinterpret_cast<uint32_t*>(header+16);
+		uint32_t mipmapcount = *reinterpret_cast<uint32_t*>(header+28);
+		uint32_t fourCC = *reinterpret_cast<uint32_t*>(header+84);
+		uint32_t blockSize = 16;
+		uint32_t size = ((width + 3)/4)*((height + 3)/4)*blockSize;
+		uint32_t format = GL_COMPRESSED_RGBA_S3TC_DXT1_EXT;
+		delete[] header;
+		switch(fourCC){
+			case 0x31545844: // DXT1
+				format = GL_COMPRESSED_RGBA_S3TC_DXT1_EXT;
+				blockSize = 8;
+				break;
+			case 0x33545844: // DXT3
+				format = GL_COMPRESSED_RGBA_S3TC_DXT3_EXT;
+				break;
+			case 0x35545844: // DXT5
+				format = GL_COMPRESSED_RGBA_S3TC_DXT5_EXT;
+				break;
+			default:
+				std::cout << "Not DXT" << std::endl;
+				return -1;
+				break;
+		}
+		uint8_t *data = new uint8_t[size];
+		for (uint32_t level=0;level<mipmapcount;level++){
+			std::cout << size << std::endl;
+			delete[] data;
+			data = new uint8_t[size];
+			gzread(gzhandle,data,size);
+			glCompressedTexImage2D(GL_TEXTURE_2D,level,format,width,height,0,size,data);
+			width = std::max(width/2, 1U);
+			height = std::max(height/2,1U);
+			size = ((width + 3)/4)*((height + 3)/4)*blockSize;
+		}
+		gzclose(gzhandle);
+		delete[] data;
+	}
+	return 0;
+}
+
 int main(void){
 	GLFWwindow* window;
 	if (!glfwInit()){
@@ -37,7 +97,7 @@ int main(void){
 	glfwWindowHint(GLFW_GREEN_BITS, mode->greenBits);
 	glfwWindowHint(GLFW_BLUE_BITS, mode->blueBits);
 	glfwWindowHint(GLFW_REFRESH_RATE, mode->refreshRate);
-	window = glfwCreateWindow(mode->width,mode->height,"Starry Sky", monitor, NULL);
+	window = glfwCreateWindow(1920,1080,"Starry Sky", monitor, NULL);
 	if (!window){
 		glfwTerminate();
 		return 2;
@@ -46,9 +106,9 @@ int main(void){
 	glfwSetWindowUserPointer(window,&camera);
 	float vertices[] = {
 		 0.f,0.f,0.0f,0.0f,
-		 900.f,0.f,2.0f,0.0f,
-		 900.f,400.f,2.0f,2.0f,
-		 0.f,400.f,0.0f,2.0f
+		 2000.f,0.f,1.0f,0.0f,
+		 2000.f,1000.f,1.0f,1.0f,
+		 0.f,1000.f,0.0f,1.0f
 	};
 	GLuint vao;
 	glGenVertexArrays(1,&vao);
@@ -79,14 +139,10 @@ int main(void){
 	glVertexAttribPointer(texAttrib,2,GL_FLOAT,GL_FALSE,4*sizeof(float),(void*)(2*sizeof(float)));
 
 	GLuint tex;
+	glm::mat4 m(1.0f);
 	glGenTextures(1,&tex);
-	int width, height;
 	glActiveTexture(GL_TEXTURE0);
-	glBindTexture(GL_TEXTURE_2D,tex);
-	unsigned char* image = stbi_load("img_test.png",&width,&height,0,STBI_rgb_alpha);
-	glTexImage2D(GL_TEXTURE_2D,0,GL_RGBA,width,height,0,GL_RGBA,GL_UNSIGNED_BYTE,image);
-	glGenerateMipmap(GL_TEXTURE_2D);
-	stbi_image_free(image);
+	loadDDSgz("atlas0.dds.gz",tex);
 	glUniform1i(glGetUniformLocation(shaderProgram,"tex"),0);
 	GLuint MatrixID = glGetUniformLocation(shaderProgram,"VP");
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
@@ -94,9 +150,12 @@ int main(void){
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	glfwSetKeyCallback(window,key_callback);
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 	while (!glfwWindowShouldClose(window)){
 	/* Render here */
 		glUniformMatrix4fv(MatrixID,1,GL_FALSE,&camera.getVP()[0][0]);
+//		glUniformMatrix4fv(MatrixID,1,GL_FALSE,&m[0][0]);
 		glClearColor(0.0f,0.0f,0.0f,1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 		glDrawElements(GL_TRIANGLES,6,GL_UNSIGNED_BYTE,0);
